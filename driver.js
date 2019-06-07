fdb7651e96a9d33e9c43fa4844b308a2638f7378
Diego Mendoza <diegomf.mendoza@gmail.com>
2019-06-07 16:42:26 -0500

    ya esta

--- driver.js
+++ driver.js
@@ -9,6 +9,7 @@ $('#btn-calcular').click(function (params) {
     var kmCiudad=Number($('#txt-km-ciudad').val());
     var tiempo=Number($('#txt-tiempo').val());
     $('#txt-recorrido').text(calculoCosto(kmTotal,kmCiudad,tiempo));
+    $('#txt-recorrido-2').text(calculoCosto2(kmTotal,kmCiudad,tiempo));
 });
 
 function calculoCosto(kmTotal,kmCiudad,tiempo) {
@@ -18,7 +19,7 @@ function calculoCosto(kmTotal,kmCiudad,tiempo) {
         console.log('Precio: '+precioBruto+' PrecioTiempo: '+0 );
     }else{
         if(kmTotal==kmCiudad){
-            if(6<kmTotal){
+            if(4<=kmTotal){
                 precioBruto= precioZona+((kmTotal-1)*(factorCiudad-0.06)*precioZona);   
             }else{
                 precioBruto= precioZona+((kmTotal-1)*factorCiudad*precioZona);   
@@ -45,6 +46,27 @@ function calculoCosto(kmTotal,kmCiudad,tiempo) {
         aumento=0.5;
     }else if(aux>0.5){
         aumento=1;
+    }
+    return precioBruto;
+}
+
+/**
+ * Formula 2
+ */
+var precioBase2=2.8;
+var precioKM2=0.52;
+var precioMin2=0.12;
+
+var precioBaseCarretera2=6;
+var precioKMCarretera2=0.5;
+var precioMinCarretera2=0.10;
+
+function calculoCosto2(kmTotal,kmCiudad,tiempo) {
+    var precioBruto=0;
+    if(kmTotal==kmCiudad){
+        precioBruto=precioBruto+precioBase2+kmTotal*precioKM2+kmTotal*precioMin2;
+    }else{
+        precioBruto=precioBruto+precioBaseCarretera2+kmTotal*precioKMCarretera2+kmTotal*precioMinCarretera2;
     }
     return precioBruto;
 }
